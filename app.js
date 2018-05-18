11793114caaaf803a3662381e2a8c2ec7e32a3a7
elharony <p.yahyaelharony@gmail.com>
2018-05-18 16:34:37 +0200

    feat: Set a counter for each image

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 // Cache Elements
-const clickCounter = document.querySelector("#counter");
 const imgs = document.querySelectorAll(".imgToClick");
 const names = document.querySelectorAll(".name");
+const counters = document.querySelectorAll(".counter");
 
 // URLs & Names
 const urlsArr = ["https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX_400x400.jpeg", "https://data.whicdn.com/images/35983568/large.jpg"];
 const namesArr = ["Emergency Kitten", "Cute Cat"];
-
-// Set the initial value to `0`
-let counter = 0;
-clickCounter.innerHTML = counter;
 
 for(let i = 0; i < imgs.length; i++) {
 
@@ -21,8 +17,7 @@ for(let i = 0; i < imgs.length; i++) {
 
     // Add `1` to the counter after clicking on an image
     imgs[i].addEventListener("click", () => {
-        counter++;
-        clickCounter.innerHTML = counter; 
+        counters[i].innerHTML++;
     });
     
 }
