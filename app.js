e58f39394a02ee3ca10025c3ebdb42c6573ff009
elharony <p.yahyaelharony@gmail.com>
2018-05-18 16:24:18 +0200

    feat: Use 2 images instead of 1

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+// Cache Elements
 const clickCounter = document.querySelector("#counter");
-const img = document.querySelector("#imgToClick");
+const imgs = document.querySelectorAll(".imgToClick");
+const names = document.querySelectorAll(".name");
+
+// URLs & Names
+const urlsArr = ["https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX_400x400.jpeg", "https://data.whicdn.com/images/35983568/large.jpg"];
+const namesArr = ["Emergency Kitten", "Cute Cat"];
 
 // Set the initial value to `0`
 let counter = 0;
 clickCounter.innerHTML = counter;
 
-// Add `1` to the counter after clicking on the image
-img.addEventListener("click", () => {
-    counter++;
-    clickCounter.innerHTML = counter; 
-});
+for(let i = 0; i < imgs.length; i++) {
+
+    // Cat URL
+    imgs[i].setAttribute("src", urlsArr[i]);
+
+    // Cat Name
+    names[i].innerHTML = namesArr[i];
+
+    // Add `1` to the counter after clicking on an image
+    imgs[i].addEventListener("click", () => {
+        counter++;
+        clickCounter.innerHTML = counter; 
+    });
+    
+}
